b559c1e9699fee3790919932e75b4b8d373b1802
niyeliscoplin <54688847+niyeliscoplin@users.noreply.github.com>
2021-05-27 00:52:49 -0400

    Add files via upload

---
+++ Background.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default class Background extends React.Component {
+	componentDidMount() {
+		new WOW.WOW().init();
+	}
+
+	render() {
+		return (
+			<div id="my-background" className="background">
+				<div id="stars" />
+				<div id="stars2" />
+				<div id="stars3" />
+				<div className="top-container flex">
+					<h1>
+						Hello, I'm Niyelis Coplin.
+						<br />
+						I'm a Full-Time Student at Lehman College.
+					</h1>
+					<button
+						className={"work-button " + this.props.bounceIn}
+						data-wow-offset="0"
+						onClick={() => {
+							let node = document.getElementById(
+								this.props.aboutRef.current.props.id
+							);
+							window.scrollTo({
+								top: node.offsetTop,
+								behavior: "smooth"
+							});
+						}}>
+						All about me! 
+					</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+Background.propTypes = {
+	aboutRef: PropTypes.object,
+	bounceIn: PropTypes.string
+};
